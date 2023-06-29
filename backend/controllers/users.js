f0d3d8a418160574eb88ed1e22e3d98de81b2696
Seyed Iman Modarres Sadeghi <imanmodarres1381@gmail.com>
2023-06-29 18:15:28 -0400

    added asyncHandler middleware

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,44 +2,38 @@ import User from "../models/User.js";
 
 import ErrorResponse from "../utils/errorResponse.js";
 
+import asyncHandler from "../middleware/async.js";
+
 // @desc   gets all users
 // @route  GET /api/v1/users/
 // @access Private
-export const getUsers = async (req, res, next) => {
-  try {
-    const users = await User.find();
+export const getUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find();
 
-    res.status(200).json({
-      success: true,
-      count: users.length,
-      data: users,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: users.length,
+    data: users,
+  });
+});
 
 // @desc   gets a user
 // @route  GET /api/v1/users/:userId
 // @access Private
-export const getUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.userId);
+export const getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.userId);
 
-    //if it is correctly formatted id but it is not in the database it will result enter this if block(we dont want to send a response with a true success and data being null in this case)
-    if (!user)
-      return next(
-        new ErrorResponse(`User not found with id of ${req.params.userId}`, 404)
-      );
+  //if it is correctly formatted id but it is not in the database it will result enter this if block(we dont want to send a response with a true success and data being null in this case)
+  if (!user)
+    return next(
+      new ErrorResponse(`User not found with id of ${req.params.userId}`, 404)
+    );
 
-    res.status(200).json({
-      success: true,
-      data: user,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
 
 // @desc   gets a email and password and if a user with this details was found it will respond the user otherwise responds in error
 // @route  POST /api/v1/users/login
@@ -54,55 +48,43 @@ export const validateUser = (req, res, next) => {
 // @desc   create a new user
 // @route  POST /api/v1/users/
 // @access Public
-export const createUser = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body);
+export const createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: user,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: user,
+  });
+});
 
 // @desc   update a user
 // @route  PUT /api/v1/users/:userId
 // @access Private
-export const updateUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
-      // if the new key is set to true we will get back the object after update
-      new: true,
-      runValidators: true,
-    });
+export const updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+    // if the new key is set to true we will get back the object after update
+    new: true,
+    runValidators: true,
+  });
 
-    if (!user)
-      return next(
-        new ErrorResponse(`User not found with id of ${req.params.userId}`, 404)
-      );
+  if (!user)
+    return next(
+      new ErrorResponse(`User not found with id of ${req.params.userId}`, 404)
+    );
 
-    res.status(200).json({ success: true, data: user });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({ success: true, data: user });
+});
 
 // @desc   delete a user
 // @route  DELETE /api/v1/users/:userId
 // @access Private
-export const deleteUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.userId);
+export const deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.userId);
 
-    if (!user)
-      return next(
-        new ErrorResponse(`User not found with id of ${req.params.userId}`, 404)
-      );
+  if (!user)
+    return next(
+      new ErrorResponse(`User not found with id of ${req.params.userId}`, 404)
+    );
 
-    res.status(200).json({ success: true, data: user });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({ success: true, data: user });
+});
