ceefd78fbf2d41ff64270cc4b172f8f7ea800646
Seyed Iman Modarres Sadeghi <imanmodarres1381@gmail.com>
2023-08-27 12:51:38 -0400

    updated shareProfileButton

--- frontend/src/components/ShareProfileButton.jsx
+++ frontend/src/components/ShareProfileButton.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Tooltip from "@mui/material/Tooltip";
 import Zoom from "@mui/material/Zoom";
 import IconButton from "@mui/material/IconButton";
 import ShareIcon from "@mui/icons-material/Share";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
 const ShareProfileButton = ({ userID }) => {
+  const [isCopiedMessageOpen, setIsCopiedMessageOpen] = useState(false);
+
   const shareIconClickHandler = () => {
     navigator.clipboard.writeText(
       `${window.location.origin}/profile/${userID}`
     );
+
+    setIsCopiedMessageOpen(true);
+  };
+
+  const closeCopiedMessageHandler = () => {
+    setIsCopiedMessageOpen(false);
   };
 
   return (
@@ -29,6 +39,21 @@ const ShareProfileButton = ({ userID }) => {
           <ShareIcon />
         </IconButton>
       </Tooltip>
+      <Snackbar
+        open={isCopiedMessageOpen}
+        autoHideDuration={4000}
+        onClose={closeCopiedMessageHandler}
+      >
+        <Alert
+          severity="success"
+          elevation={6}
+          variant="filled"
+          sx={{ width: "100%" }}
+          onClose={closeCopiedMessageHandler}
+        >
+          Link copied!
+        </Alert>
+      </Snackbar>
     </>
   );
 };
